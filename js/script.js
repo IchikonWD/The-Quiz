a8f96aa77d3332bf4650b3623c3fe39197106d7a
IchikonWD <ezequielmigueles@icloud.com>
2021-07-21 23:02:40 +0200

    api fetch created

--- js/script.js
+++ js/script.js
@@ -5,3 +5,41 @@ function load_home() {
 }
 
 document.getElementById("play-btn").onclick = load_home;
+
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple"
+)
+  .then((res) => res.json())
+  .then((json) => {
+    let arr = json.results;
+    let questions = [];
+    for (let i = 0; i < arr.length; i++) {
+      questions.push(arr[i].question);
+    }
+    let corectAnswers = [];
+    for (let i = 0; i < arr.length; i++) {
+      corectAnswers.push(arr[i].correct_answer);
+    }
+    let incorrectAnswers = [];
+    for (let i = 0; i < arr.length; i++) {
+      incorrectAnswers.push(arr[i].incorrect_answers);
+    }
+
+    class quizQuestions {
+      constructor(question, correctAnswer, incorrectAnswers) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswers = incorrectAnswers;
+      }
+    }
+    let quiz = [];
+    for (let i = 0; i < arr.length; i++) {
+      quiz.push(
+        new quizQuestions(questions[i], corectAnswers[i], incorrectAnswers[i])
+      );
+    }
+    console.log(quiz[0].question);
+})
+.catch((err) => {
+    console.log(err);
+});
